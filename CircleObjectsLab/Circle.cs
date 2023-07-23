7b63d18642dedf9be9db34897be13148d597db2f
Omar Abdulla <osabdulla@gmail.com>
2023-07-23 01:22:39 -0400

    First

---
+++ CircleObjectsLab/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjectsLab
+{
+    internal class Circle
+    {
+        //Properties
+        private double Radius { get; set; }
+        //Constructor
+        public Circle(double _radius)
+        {
+            Radius = _radius;
+        }
+        //Methods
+        public double CalculateDiameter(double diameter)
+        {
+            return diameter = 2 * Radius;
+        }
+        public double CalculateCircumference(double circum)
+        {
+            return circum = 2 * Math.PI * Radius;
+        }
+        public double CalculateArea(double area)
+        {
+            return area = Math.PI * (Radius * Radius);
+        }
+        public void Grow()
+        {
+            Console.WriteLine("The circle is magically growing.");
+        }
+        public double GetRadius(double get)
+        {
+            return Radius * 2;
+        }
+    }
+}
---
+++ CircleObjectsLab/Program.cs
@@ -0,0 +1,79 @@
+﻿// Circle Objects Lab
+
+using CircleObjectsLab;
+
+Console.WriteLine("Welcome to the Circle Tester\n");
+
+bool runProgram = true;
+while (runProgram)
+{
+    double radius = 0;
+    while (true)
+    {
+
+        //The application prompts the user to enter a radius; the user may enter a number with decimals (double).
+        //The application displays an error if the user enters invalid data and asks the user again for a radius.
+        try
+        {
+            Console.WriteLine("Enter radius:");
+            radius = double.Parse(Console.ReadLine().Trim());
+            if (radius <= 0)
+            {
+                throw new Exception("Error: Invalid data. Please try again");
+            }
+            else
+            {
+                break;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+
+
+
+
+    //When the user enters valid data,
+    //the application creates an instance of a Circle
+    //and then uses its methods to display the Diameter, Circumference and Area.
+
+    Circle shapeCircle = new Circle(radius);
+    Console.WriteLine($"Diameter: {shapeCircle.CalculateDiameter(radius)}");
+    Console.WriteLine($"Circumference: {shapeCircle.CalculateCircumference(radius)}");
+    Console.WriteLine($"Area: {shapeCircle.CalculateArea(radius)}");
+
+
+    //The application asks the user if the circle should grow.
+    //If the user says yes, call the grow method and then loop back to the method calls for the formulas. 
+    //The grow method will adjust radius to double its value.
+    while (true)
+    {
+        Console.WriteLine("Should the circle grow?");
+        string response = Console.ReadLine().ToLower().Trim();
+
+        if (response == "yes" || response == "y")
+        {
+            shapeCircle.Grow();
+            //shapeCircle.GetRadius(radius);
+            Console.WriteLine($"The radius grew to {shapeCircle.GetRadius(radius)}");
+            break;
+
+        }
+
+        else if (response == "no" || response == "n")
+        {
+            Console.WriteLine($"Goodbye. The circle's final radius is {radius}");
+            runProgram = false;
+            break;
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, please enter either a y/n");
+        }
+    }
+
+}
