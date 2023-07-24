26337b6d08d1fe95ae09e214596d5210ce6f771a
Omar Abdulla <osabdulla@gmail.com>
2023-07-24 17:07:12 -0400

    Validator

--- CircleObjectsLab/Program.cs
+++ CircleObjectsLab/Program.cs
@@ -2,35 +2,34 @@
 
 using CircleObjectsLab;
 
+
+
+
 Console.WriteLine("Welcome to the Circle Tester\n");
 
+//The application prompts the user to enter a radius; the user may enter a number with decimals (double).
+//The application displays an error if the user enters invalid data and asks the user again for a radius.
+
+//double radius = -1;
+
+Console.WriteLine("Enter radius:");
+double radius = Validator.GetPositiveInputInt();
+//        radius = double.Parse(Console.ReadLine().Trim());
+//        if (radius <= 0)
+//        {
+//            throw new Exception("Error: Invalid data. Please try again");
+//        }
+//        else
+//        {
+//            break;
+//        }
+//    }
+//    catch (Exception e)
+//    {
+//        Console.WriteLine(e.Message);
+//    }
 
 
-double radius = -1;
-while (true)
-{
-
-    //The application prompts the user to enter a radius; the user may enter a number with decimals (double).
-    //The application displays an error if the user enters invalid data and asks the user again for a radius.
-    try
-    {
-        Console.WriteLine("Enter radius:");
-        radius = double.Parse(Console.ReadLine().Trim());
-        if (radius <= 0)
-        {
-            throw new Exception("Error: Invalid data. Please try again");
-        }
-        else
-        {
-            break;
-        }
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-    }
-
-}
 
 
 
@@ -46,35 +45,45 @@ while (runMethods)
     Console.WriteLine($"Diameter: {shapeCircle.CalculateDiameter()}");
     Console.WriteLine($"Circumference: {shapeCircle.CalculateCircumference()}");
     Console.WriteLine($"Area: {shapeCircle.CalculateArea()}");
+    runMethods = Validator.GetContinue("Would you like the circle to grow?");
+    if (runMethods == true)
+    {
+        shapeCircle.Grow();
+    }
+    else
+    {
+        Console.WriteLine($"Goodbye! The circle radius is {shapeCircle.GetRadius()}");
+    }
+}
 
 
     //The application asks the user if the circle should grow.
     //If the user says yes, call the grow method and then loop back to the method calls for the formulas. 
     //The grow method will adjust radius to double its value.
 
-    Console.WriteLine("Should the circle grow?");
-    while (true)
-    {
-        string response = Console.ReadLine().ToLower().Trim();
+//    Console.WriteLine("Should the circle grow?");
+//    while (true)
+//    {
+//        string response = Console.ReadLine().ToLower().Trim();
 
-        if (response == "yes" || response == "y")
-        {
-            shapeCircle.Grow();
-            break;
+//        if (response == "yes" || response == "y")
+//        {
+//            shapeCircle.Grow();
+//            break;
 
-        }
+//        }
 
-        else if (response == "no" || response == "n")
-        {
-            Console.WriteLine($"Goodbye. The circle's final radius is {shapeCircle.GetRadius()}");
-            runMethods = false;
-            break;
+//        else if (response == "no" || response == "n")
+//        {
+//            Console.WriteLine($"Goodbye. The circle's final radius is {shapeCircle.GetRadius()}");
+//            runMethods = false;
+//            break;
 
-        }
-        else
-        {
-            Console.WriteLine("Invalid input, please enter either a y/n");
-        }
-    }
+//        }
+//        else
+//        {
+//            Console.WriteLine("Invalid input, please enter either a y/n");
+//        }
+//    }
 
-}
+//}
